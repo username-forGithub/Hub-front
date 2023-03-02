bb0a7beb56383e178a1a65ee6097d7518785be2b
Bria222 <techconsultants.bree@gmail.com>
2023-03-02 15:37:54 +0300

    add protected route

--- src/App.js
+++ src/App.js
@@ -8,12 +8,17 @@ import {
 } from 'react-router-dom';
 import SplashScreen from './components/Splash/Splash';
 import Home from './components/Home/Home';
+import Login from './Login/Login';
+import Register from './Register/Register';
 import ProtectedRoute from './routing/ProtectedRoute';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<SplashScreen />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/" element={<SplashScreen />} />
         <Route element={<ProtectedRoute />}>
 
