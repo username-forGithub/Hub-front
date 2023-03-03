26d36fe31c17c332ed42db8dbfde47ea5adc2ad9
Bateyjosue <josuebatey19@gmail.com>
2023-03-03 02:58:08 +0200

    Add reducer, get Tutor Details

--- src/Redux/tutor/tutorReducer.js
+++ src/Redux/tutor/tutorReducer.js
@@ -5,17 +5,24 @@ axios.defaults.baseURL = '127.0.0.1:3000';
 
 const initialState = [];
 const GET_ALL_TUTOR = 'GET_ALL_TUTOR/fulfilled';
+const GET_TUTOR_DETAILS = 'GET_TUTOR_DETAILS/fulfilled';
 
 export const getAllTutor = createAsyncThunk('GET_ALL_TUTOR', async () => {
   const response = axios.get('/api/v2/tutors');
   return response.data;
 });
 
+export const getSTutorDetails = createAsyncThunk('GET_TUTOR_DETAILS', async () => {
+  const response = axios.get('/api/v2/tutor');
+  return response.data;
+});
+
 const tutorReducer = (state = initialState, action) => {
   switch (action.type) {
-    case GET_ALL_TUTOR: return {
+    case GET_ALL_TUTOR: return [
       ...state, ...action.payload,
-    };
+    ];
+    case GET_TUTOR_DETAILS: return { ...state, ...action.payload };
     default: {
       return state;
     }
