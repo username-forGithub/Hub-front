665833c86c3ca43217327dddb5120d7bc469fe5c
Brian Nyachae <briantechologies.co.ke@gmail.com>
2023-03-10 15:22:49 +0300

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,4 @@
-/* eslint-disable linebreak-style */
-/* eslint-disable react/jsx-filename-extension */
-/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable */
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import './tutors.css';
