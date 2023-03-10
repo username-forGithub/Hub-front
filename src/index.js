41d4655493267bc0ba2b66dc9420c1d446f5496c
Bria222 <techconsultants.bree@gmail.com>
2023-03-10 14:03:16 +0300

    add test for booking  apointment

--- src/index.js
+++ src/index.js
@@ -4,16 +4,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { Provider } from 'react-redux';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
-import store from './Redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
 );
