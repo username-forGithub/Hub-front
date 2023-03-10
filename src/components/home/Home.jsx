f73be1c63efe19a76ee5b536c7f00778eb748fc9
Kene Agagwu <keneagagwuu@gmail.com>
2023-03-10 13:10:01 +0100

    Update Home.jsx

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable */
 /* eslint-disable import/no-extraneous-dependencies */
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
