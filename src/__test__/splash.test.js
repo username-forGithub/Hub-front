9dac820896d7531ab2a1bbe9d1ee348149e8f8d0
Bria222 <techconsultants.bree@gmail.com>
2023-03-10 11:35:54 +0300

    add tests for splash screen

---
+++ src/__test__/splash.test.js
@@ -0,0 +1,49 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable import/extensions */
+/* eslint-disable linebreak-style */
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import SplashScreen from '../components/Splash/Splash';
+import store from '../redux/configureStore';
+
+describe('Splash screen tests', () => {
+  it('renders correctly', () => {
+    const tree = render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <SplashScreen />
+        </BrowserRouter>
+      </Provider>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('Splash page has a sign up link', () => {
+    const register = render(
+      <>
+        <Provider store={store}>
+          <BrowserRouter>
+            <SplashScreen />
+          </BrowserRouter>
+        </Provider>
+      </>,
+    );
+
+    expect(register.findByText('Sign up')).toBeTruthy();
+  });
+
+  it('Splash page has a log in link', () => {
+    const register = render(
+      <>
+        <Provider store={store}>
+          <BrowserRouter>
+            <SplashScreen />
+          </BrowserRouter>
+        </Provider>
+      </>,
+    );
+
+    expect(register.findByText('Log in')).toBeTruthy();
+  });
+});
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
+/* eslint-disable linebreak-style */
 /* eslint-disable comma-dangle */
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { Provider } from 'react-redux';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
+import store from './Redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
